823129d37540f4b305f13fb75a4a90a61ce37099
Welerson Freitas <wfggames@hotmail.com>
2026-04-19 20:21:18 -0300

    chore: security - cleaning sensible data

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  MOCK_API_URL: 'https://666dc8b57a3738f7cacd4df5.mockapi.io/api/v1',
-  COGNITO_URL: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_ZSsg71Nvz',
-  COGNITO_CLIENT_ID: '3ulumjidr5h2o3eci29dqduj83',
-  COGNITO_SCOPE: 'phone openid email',
-  COGNITO_RESPONSE_TYPE: 'code',
-  COGNITO_REDIRECT_URL: 'http://localhost:4200',
+  MOCK_API_URL: 'd',
+  COGNITO_URL: 'd',
+  COGNITO_CLIENT_ID: 'd',
+  COGNITO_SCOPE: 'd',
+  COGNITO_RESPONSE_TYPE: 'd',
+  COGNITO_REDIRECT_URL: 'd',
 };
